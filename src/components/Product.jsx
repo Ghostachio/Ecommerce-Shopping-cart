61577d297208f315d2951aac08573e14ab0c7635
Ghostachio <mansour.hamza2.0@gmail.com>
2023-11-24 12:16:08 +0100

    fixed images

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,20 +1,20 @@
-import "../assets/image-product-1.jpg"
-import "../assets/image-product-1-thumbnail.jpg"
-import "../assets/image-product-2-thumbnail.jpg"
-import "../assets/image-product-3-thumbnail.jpg"
-import "../assets/image-product-4-thumbnail.jpg"
+import image1 from "../assets/image-product-1.jpg"
+import image2 from "../assets/image-product-1-thumbnail.jpg"
+import image3 from "../assets/image-product-2-thumbnail.jpg"
+import image4 from "../assets/image-product-3-thumbnail.jpg"
+import image5 from "../assets/image-product-4-thumbnail.jpg"
 
 const Product = () => {
   return (
     <div className="flex flex-col p-6 justify-center gap-5 pt-8 ">
       <div className="flex justify-center ">
-        <img className="rounded-lg" src="../src/assets/image-product-1.jpg"  height={400} width={400} alt="1" />
+        <img className="rounded-lg" src={image1}  height={400} width={400} alt="1" />
       </div>
       <div className="flex gap-4 w-[500px] flex-wrap justify-center">
-        <img className="rounded-lg" src="../assets/image-product-1-thumbnail.jpg" height={100} width={100}  alt="2" />
-        <img className="rounded-lg" src="../assets/image-product-2-thumbnail.jpg" height={100} width={100}  alt="3" />
-        <img className="rounded-lg" src="../assets/image-product-3-thumbnail.jpg" height={100} width={100}  alt="4" />
-        <img className="rounded-lg" src="../assets/image-product-4-thumbnail.jpg" height={100} width={100}  alt="5" />
+        <img className="rounded-lg" src={image2} height={100} width={100}  alt="2" />
+        <img className="rounded-lg" src={image3} height={100} width={100}  alt="3" />
+        <img className="rounded-lg" src={image4} height={100} width={100}  alt="4" />
+        <img className="rounded-lg" src={image5} height={100} width={100}  alt="5" />
       </div>
     </div>
   )
