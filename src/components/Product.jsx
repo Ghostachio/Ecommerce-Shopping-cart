b58b7c0d6db3f09cdf3a0b8620377375b7380073
Ghostachio <mansour.hamza2.0@gmail.com>
2023-11-24 12:07:19 +0100

    fixed images

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -11,10 +11,10 @@ const Product = () => {
         <img className="rounded-lg" src="../src/assets/image-product-1.jpg"  height={400} width={400} alt="1" />
       </div>
       <div className="flex gap-4 w-[500px] flex-wrap justify-center">
-        <img className="rounded-lg" src="../src/assets/image-product-1-thumbnail.jpg" height={100} width={100}  alt="2" />
-        <img className="rounded-lg" src="../src/assets/image-product-2-thumbnail.jpg" height={100} width={100}  alt="3" />
-        <img className="rounded-lg" src="../src/assets/image-product-3-thumbnail.jpg" height={100} width={100}  alt="4" />
-        <img className="rounded-lg" src="../src/assets/image-product-4-thumbnail.jpg" height={100} width={100}  alt="5" />
+        <img className="rounded-lg" src="../assets/image-product-1-thumbnail.jpg" height={100} width={100}  alt="2" />
+        <img className="rounded-lg" src="../assets/image-product-2-thumbnail.jpg" height={100} width={100}  alt="3" />
+        <img className="rounded-lg" src="../assets/image-product-3-thumbnail.jpg" height={100} width={100}  alt="4" />
+        <img className="rounded-lg" src="../assets/image-product-4-thumbnail.jpg" height={100} width={100}  alt="5" />
       </div>
     </div>
   )
